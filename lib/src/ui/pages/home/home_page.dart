4796621ef5f7cd6a01758c7bdd89b2bc6d5dc552
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2022-06-18 19:17:53 -0300

    corrigindo barra de pesquisa

--- lib/src/ui/pages/home/home_page.dart
+++ lib/src/ui/pages/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:movie_list/src/ui/pages/home/components/movie_lists.dart';
 import 'package:movie_list/src/ui/pages/home/components/result_reader.dart';
 import 'package:movie_list/src/ui/pages/home/components/search_result.dart';
 import 'package:movie_list/src/ui/widgets/page_nav.dart';
+import 'package:movie_list/src/ui/widgets/search_box.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,10 +15,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final HomeBloc _homeBloc;
   @override
+
   void initState() {
     super.initState();
-    context.read<HomeBloc>().add(GetMovieLists());
+    _homeBloc = context.read<HomeBloc>()..add(GetMovieLists());
   }
 
   @override
@@ -27,34 +30,43 @@ class _HomePageState extends State<HomePage> {
         title: const Text('Movies Catalog'),
         centerTitle: true,
       ),
-      body: BlocBuilder<HomeBloc, HomeState>(
-        builder: (_, state) {
-          if (state is LoadingMovieLists) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is FechedMovieLists) {
-            return MovieLists(
-              mostPopularMovies: state.mostPopular.results,
-              topRatedMovies: state.topRated.results,
-            );
-          } else if (state is LoadingSearchResult) {
-            return Column(
-              children: [
-                ResultHeader(searchTerm: state.searchTerm),
-                const Expanded(child: Center(child: CircularProgressIndicator()))
-              ],
-            );
-          } else if (state is FetchedSearchResult) {
-            return ListView(
-              children: [
-                ResultHeader(searchTerm: state.searchTerm),
-                SearchResult(movieList: state.searchResult.results),
-                PageNav(limit: state.searchResult.totalPages),
-              ],
-            );
-          }
-          var errorState = state as HomeErrorState;
-          return Center(child: Text(errorState.error.toString()));
-        },
+      body: Column(
+        children: [
+          SearchBar(
+            onSearch: (text) => _homeBloc.add(SearchMovies(searchTerm: text)),
+          ),
+          Expanded(
+            child: BlocBuilder<HomeBloc, HomeState>(
+              builder: (_, state) {
+                if (state is LoadingMovieLists) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is FechedMovieLists) {
+                  return MovieLists(
+                    mostPopularMovies: state.mostPopular.results,
+                    topRatedMovies: state.topRated.results,
+                  );
+                } else if (state is LoadingSearchResult) {
+                  return Column(
+                    children: [
+                      ResultHeader(searchTerm: state.searchTerm),
+                      const Expanded(child: Center(child: CircularProgressIndicator()))
+                    ],
+                  );
+                } else if (state is FetchedSearchResult) {
+                  return ListView(
+                    children: [
+                      ResultHeader(searchTerm: state.searchTerm),
+                      SearchResult(movieList: state.searchResult.results),
+                      PageNav(limit: state.searchResult.totalPages),
+                    ],
+                  );
+                }
+                var errorState = state as HomeErrorState;
+                return Center(child: Text(errorState.error.toString()));
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/src/ui/widgets/search_box.dart
+++ lib/src/ui/widgets/search_box.dart
@@ -1,48 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:movie_list/src/ui/l10n/app_localizations.dart';
 
-class SearchBar extends StatelessWidget {
+class SearchBar extends StatefulWidget {
   const SearchBar({
-    Key? key,
-  }) : super(key: key);
+    super.key,
+    required this.onSearch,
+  });
+
+  final void Function(String) onSearch;
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // final TextEditingController inputController = TextEditingController();
-    // final SearchController searchController = Provider.of<SearchController>(
-    //   context,
-    // );
-
-    // return Container(
-    //   padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
-    //   color: const Color(0xff0c0c0c),
-    //   child: Focus(
-    //     onFocusChange: (hasFocus) {
-    //       if (!hasFocus) searchController.start(inputController.text);
-    //     },
-    //     child: TextField(
-    //       controller: inputController,
-    //       decoration: InputDecoration(
-    //         enabledBorder: const UnderlineInputBorder(
-    //           borderSide: BorderSide(color: Colors.grey),
-    //         ),
-    //         hintText: AppLocalizations.of(context).sbHint,
-    //         hintStyle: TextStyle(color: Colors.grey.shade800),
-    //         contentPadding: const EdgeInsets.only(left: 10, top: 15),
-    //         suffixIcon: IconButton(
-    //           icon: const Icon(
-    //             Icons.search,
-    //             color: Colors.grey,
-    //           ),
-    //           padding: const EdgeInsets.symmetric(
-    //             horizontal: 10,
-    //             vertical: 10,
-    //           ),
-    //           onPressed: () => searchController.start(inputController.text),
-    //         ),
-    //       ),
-    //     ),
-    //   ),
-    // );
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 14),
+      color: const Color(0xff0c0c0c),
+      child: Focus(
+        onFocusChange: (hasFocus) {
+          if (!hasFocus) widget.onSearch(_controller.text);
+        },
+        child: TextField(
+          controller: _controller,
+          decoration: InputDecoration(
+            enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            hintText: AppLocalizations.of(context).sbHint,
+            hintStyle: TextStyle(color: Colors.grey.shade800),
+            contentPadding: const EdgeInsets.only(left: 10, top: 15),
+            suffixIcon: IconButton(
+              icon: const Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 10,
+              ),
+              onPressed: () => widget.onSearch(_controller.text),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
