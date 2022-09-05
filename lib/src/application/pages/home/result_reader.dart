d42d22677d5bd6bcb278c045514418810021fc36
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2022-09-05 19:56:55 -0300

    utilizando os novos cubits na home page

--- lib/src/application/pages/home/result_reader.dart
+++ lib/src/application/pages/home/result_reader.dart
@@ -1,49 +1,55 @@
 part of 'home_page.dart';
 
 class _ResultHeader extends StatefulWidget {
-  const _ResultHeader({ Key? key, required this.searchTerm}) : super(key: key);
-
-  final String searchTerm;
+  const _ResultHeader();
 
   @override
   State<_ResultHeader> createState() => _ResultHeaderState();
 }
 
 class _ResultHeaderState extends State<_ResultHeader> {
-  late final HomeBloc _homeBloc;
+  late final SearchForMoviesCubit _searchForMoviesCubit;
 
   @override
   void initState() {
     super.initState();
-    _homeBloc = context.read();
+    _searchForMoviesCubit = context.read();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-      child: Row(
-        children: [
-          Expanded(
-            child: fieldTitle(
-              AppLocalizations.of(context).resultsHeader + widget.searchTerm,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 20),
-            child: IconButton(
-              splashRadius: 15.0,
-              onPressed: () => _homeBloc.add(GetMovieLists()),
-              icon: const Icon(
-                Icons.clear,
-                color: Colors.red,
-                semanticLabel: 'Cancel search',
+    return BlocBuilder<SearchForMoviesCubit, SearchForMoviesState>(
+      builder: (context, state) {
+        if (!state.searching) {
+          return nil;
+        }
+        final searchTerm = state.searchTerm!;
+        return Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+          child: Row(
+            children: [
+              Expanded(
+                child: fieldTitle(
+                  AppLocalizations.of(context).resultsHeader + searchTerm,
+                ),
               ),
-              visualDensity: VisualDensity.compact,
-            ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: IconButton(
+                  splashRadius: 15.0,
+                  onPressed: () => _searchForMoviesCubit.cancelSearch(),
+                  icon: const Icon(
+                    Icons.clear,
+                    color: Colors.red,
+                    semanticLabel: 'Cancel search',
+                  ),
+                  visualDensity: VisualDensity.compact,
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      }
     );
   }
 }
