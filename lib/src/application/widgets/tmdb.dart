bb1e4942f251e7a468a5d0dc0605874b66ef52bc
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2022-09-09 21:03:57 -0300

    reduzindo resolucao das imagens

--- lib/src/application/widgets/tmdb.dart
+++ lib/src/application/widgets/tmdb.dart
@@ -1,7 +1,7 @@
 library movie_list.tmdb;
 
 String baseApiUrl = 'https://api.themoviedb.org/3';
-String baseImagesUrl = 'https://image.tmdb.org/t/p/original';
+String baseImagesUrl = 'https://image.tmdb.org/t/p/w200';
 String key = 'c20f47180f2db697f0fcd8ac821fb85c';
 String lang = 'en-US';
 String includeAdult = 'false';
